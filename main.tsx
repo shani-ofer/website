88fe5347959559f3cac172f109c89d2a74b958d9
shani-ofer <shani.ofer@gmail.com>
2026-03-19 22:42:29 +0200

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -4,10 +4,10 @@
  */
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Layout } from "./Layout";
-import { Homepage } from "./Homepage";
-import { IndividualsPage } from "./IndividualsPage";
-import { CompaniesPage } from "./CompaniesPage";
+import { Layout } from "@/src/components/Layout";
+import { Homepage } from "@/src/pages/Homepage";
+import { IndividualsPage } from "@/src/pages/IndividualsPage";
+import { CompaniesPage } from "@/src/pages/CompaniesPage";
 
 export default function App() {
   return (
